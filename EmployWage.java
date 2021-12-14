0d84487217188f6e3943f2f417e606eaaead6ed0
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-14 20:56:51 +0530

    Added part time employee also and calculate employ wage

--- EmployWage.java
+++ EmployWage.java
@@ -1,10 +1,12 @@
 
 public class EmployWage {
-    public static final int full_time = 1;
+    public static final int part_time = 1;
+    public static final int full_time = 2;
     public static final int emp_rate = 20;
 
+
     public static int attendance() {
-        int check = (int) Math.floor(Math.random() * 10) % 2;
+        int check = (int) Math.floor(Math.random() * 10) % 3;
         return check;
     }
 
@@ -12,6 +14,8 @@ public class EmployWage {
         int emp_hrs;
         if (attend_status == full_time)
             emp_hrs = 8;
+        else if (attend_status == part_time)
+            emp_hrs = 4;
         else
             emp_hrs = 0;
         return emp_hrs;
