e0b9af577548d31d51793d41fd8170e4c9e57132
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-14 21:25:13 +0530

    Calculate wages for a month assuming 20 work days in a month

--- EmployWage.java
+++ EmployWage.java
@@ -3,7 +3,7 @@ public class EmployWage {
     public static final int part_time = 1;
     public static final int full_time = 2;
     public static final int emp_rate = 20;
-
+    public static final int work_day=20;
 
     public static int attendance() {
         int check = (int) Math.floor(Math.random() * 10) % 3;
@@ -24,17 +24,26 @@ public class EmployWage {
         }
         return emp_hrs;
     }
-    public static int calEmpwage( int emp_hours) {
-        int wage = emp_hours * emp_rate;
-        return wage;
+    public static int calEmpwage( ) {
+        int tot_empwage=0, wagePerDay=0;
+        for( int i=0; i<work_day; i++) {
+            int emp_attendance = attendance();
+            System.out.println("Employ Attendance Status=" + emp_attendance);
+
+            int emp_hoursPerDay = calEmpHours(emp_attendance);
+
+            wagePerDay = emp_hoursPerDay * emp_rate;
+            System.out.println("Employ wage per day = " + wagePerDay);
+
+            tot_empwage += wagePerDay;
+        }
+        return tot_empwage;
     }
+
     public static void main(String[] args) {
-        int emp_attendance = attendance();
-        System.out.println("Employ Attendance Status=" + emp_attendance);
 
-        int emp_hoursPerDay = calEmpHours(emp_attendance);
+        int emp_wage = calEmpwage();
+        System.out.println("Total employ wage = " + emp_wage);
 
-        int emp_wage = calEmpwage(emp_hoursPerDay);
-        System.out.println("Employ wage= " + emp_wage);
     }
 }
